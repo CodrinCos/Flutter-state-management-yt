e73c78d43628ffbec10be51c1b6c899a87227728
CodrinCos <codrin.costea22@gmail.com>
2022-09-29 18:21:14 +0200

    step-3

--- flutterhooks/lib/main.dart
+++ flutterhooks/lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+//this takes out null values from a list
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
+      map(transform ?? (e) => e).where((element) => element != null).cast();
+}
+
+void testIt() {
+  final values = [1, 2, null, 3];
+  final nullValues = values.compactMap();
+}
 
 void main() {
   runApp(
@@ -13,33 +25,29 @@ void main() {
   );
 }
 
+const url = "";
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
+    final future = useMemoized(
+      (() => NetworkAssetBundle(Uri.parse(url))
+          .load(url)
+          .then((data) => data.buffer.asUint8List())
+          .then((data) => Image.memory(data))),
     );
+
+    final snapshot = useFuture(future);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("HomePage"),
       ),
-      body: Column(children: [
-        TextField(
-          controller: controller,
-        ),
-        Text('You typed ${text.value}'),
-      ]),
+      body: Column(
+        children: [snapshot.data].compactMap().toList(),
+      ),
     );
   }
 }
