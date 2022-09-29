4bc6a3aaf4497f059de7cad61034ed29f9a81341
CodrinCos <codrin.costea22@gmail.com>
2022-09-29 16:59:53 +0200

    step2

--- flutterhooks/lib/main.dart
+++ flutterhooks/lib/main.dart
@@ -13,22 +13,33 @@ void main() {
   );
 }
 
-Stream<String> getTime() => Stream.periodic(
-      const Duration(seconds: 1),
-      (_) => DateTime.now().toIso8601String(),
-    );
-
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
+    final controller = useTextEditingController();
+    final text = useState('');
+    useEffect(
+      () {
+        controller.addListener(() {
+          text.value = controller.text;
+        });
+        return null;
+      },
+      [controller],
+    );
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(dateTime.data ?? "HomePage"),
+        title: const Text("HomePage"),
       ),
+      body: Column(children: [
+        TextField(
+          controller: controller,
+        ),
+        Text('You typed ${text.value}'),
+      ]),
     );
   }
 }
